5d22977d477388fa0d0660a83d34f5fa766812d6
Sandy <elecdeep9754@gmail.com>
2025-09-17 13:16:18 +0530

    second version of data saved

--- mycode.py
+++ mycode.py
@@ -3,15 +3,21 @@ import os
 # create a sample DataFramewith column names
 data = {'Name': ['Alice', 'Bob', 'Charlie'],
        'Age': [25, 30, 35],
-        'City': ['New York', 'Los Angeles', 'Chicago']}
+       'City': ['New York', 'Los Angeles', 'Chicago']}
 df = pd.DataFrame(data)
+
+# Adding new row to df for v2 
+new_row_loc = ('Name': 'GF1', 'Age': 30, 'city':'city1')
+df.loc[len(df.index)] = new_row_loc
 
 data_dir = 'data'
 os.makedirs(data_dir,exist_ok=True)
-#Define the file path
-file_path=os.path.join(data_dir,'sample_data.csv')
-
+#Define the file p ath
+file_path=o s.path.join(data_dir,'sample_data.csv')
+ 
 #save the DataFrame to a csv file, including column names
+
 df.to_csv(file_path,index=False)
 
 print(f"csv file saved to {file_path}")
+  
