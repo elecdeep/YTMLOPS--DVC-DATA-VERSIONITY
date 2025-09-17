d7e5b1a24fe05f720ba99e4bba137d2149c904ab
Sandy <elecdeep9754@gmail.com>
2025-09-17 17:55:53 +0530

    third version of dara saved

--- mycode.py
+++ mycode.py
@@ -14,8 +14,8 @@ new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)]   = new_row_loc
 
 # # Adding new row to df for V3
-#new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
-#df.loc[len(df.index)] = new_row_loc2
+new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
@@ -27,4 +27,4 @@ file_path = os.path.join(data_dir, 'sample_data.csv')
 # Save the DataFrame to a CSV file, including column names
 df.to_csv(file_path, index=False)
 
-print(f"CSV file saved to {file_path}")
+print(f"CSV file saved to {file_path}")   
